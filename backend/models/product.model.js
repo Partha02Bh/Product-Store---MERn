3a915f4b81103c4339f18b91d2273d5e94f107ec
Partha02Bh <parthabhattacharya251@gmail.com>
2024-10-29 19:57:58 +0530

    completed

---
+++ backend/models/product.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true  // whenever a product is created the timestamp will hold the record of createdAt, updatedAt
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
+
+
+
